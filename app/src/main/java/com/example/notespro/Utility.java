fd8afc51c19da5f476958a0d4f55ba7046cfdbe4
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2023-02-02 09:50:51 +0530

    Initial Commit

--- app/src/main/java/com/example/notespro/Utility.java
+++ app/src/main/java/com/example/notespro/Utility.java
@@ -3,10 +3,13 @@ package com.example.notespro;
 import android.widget.Toast;
 import android.content.Context;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class Utility {
     static void showToast(Context context, String message){
@@ -17,6 +20,10 @@ public class Utility {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
 
+    }
+
+    static  String timestampToString(Timestamp timestamp){
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
     }
 }
 
